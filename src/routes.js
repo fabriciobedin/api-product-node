5b77021a7f7f57c75196b05ab8efb6d705d8b565
Fabricio <fabricioobedin@gmail.com>
2019-09-03 23:33:35 -0300

    creating crud

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const routes = express.routes();
+
+const ProductController = require("./controllers/product-controller");
+
+routes.get("/products", ProductController.index);
+routes.get("/products/:id", ProductController.show);
+routes.post("/products", ProductController.store);
+routes.put("/products/:id", ProductController.update);
+routes.delete("/products/:id", ProductController.destroy);
+
+module.exports = routes;
